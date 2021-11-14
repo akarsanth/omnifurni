e29cb8746101e5c54e8c0fb91683bee0fa224830
AakarshanThapa <akarsan.thapa@gmail.com>
2021-11-14 15:47:49 +0545

    Frontend Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
-import logo from "./logo.svg";
 import "./App.css";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+// MUI components
+import Typography from "@mui/material/Typography";
+
+// theming
+const theme = createTheme({});
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Typography>Hello I am under the water save me</Typography>
+    </ThemeProvider>
+  );
 }
 
 export default App;
